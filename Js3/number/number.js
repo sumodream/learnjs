c14f4d3b3193170950d6ae7c2038036267dd42cd
sumo <sumo_dream@sina.com>
2015-11-04 14:40:38 +0800

    11.4

---
+++ Js3/number/number.js
@@ -0,0 +1,22 @@
+var num = 10 ;
+console.log(num.toString());			//10
+console.log(num.toString(2));			//1010
+console.log(num.toString(8));			//12
+console.log(num.toString(10));			//10
+console.log(num.toString(16));			//a
+//小数点后两位
+console.log(num.toFixed(2));			//10.00
+var num1 = 10.0005 ;
+console.log(num1.toFixed(3));			//10.001
+
+
+/**
+ * Number对象是Number类型的实例 基本类型的数值则不是
+ * @type {Number}
+ */
+var numObject = new Number(10) ;
+var numValue = 10 ;
+console.log(typeof numObject);				//object
+console.log(typeof numValue);				//number
+console.log(numObject instanceof Number);	//true
+console.log(numValue instanceof Number);	//false
